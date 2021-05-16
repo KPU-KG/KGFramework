8a4d40807befe35c03b64ed723dedfd000812c64
SpiceSoy <aksghkgh12@gmail.com>
2021-05-16 16:54:40 +0900

    add sound and change sound

--- KGFramework/KGMain/KGFramework.cpp
+++ KGFramework/KGMain/KGFramework.cpp
@@ -83,7 +83,16 @@ bool KG::GameFramework::Initialize(const EngineDesc& engineDesc, const Setting& 
 	this->sound = std::make_unique<KG::Sound::SoundManager>();
 	this->sound->Initialize();
 	this->sound->RegisterSound("Resource/Sound/shot.wav", KG::Sound::SoundType::EFFECTIVE, SOUND_EFF_SHOT);
-	this->sound->RegisterSound("Resource/Sound/reload.wav", KG::Sound::SoundType::EFFECTIVE, SOUND_EFF_RELOAD);
+    this->sound->RegisterSound("Resource/Sound/reload.wav", KG::Sound::SoundType::EFFECTIVE, SOUND_EFF_RELOAD);
+
+    this->sound->RegisterSound("Resource/Sound/VectorDraw.wav", KG::Sound::SoundType::EFFECTIVE, VECTOR_SOUND::DRAW);
+    this->sound->RegisterSound("Resource/Sound/VectorFire1.wav", KG::Sound::SoundType::EFFECTIVE, VECTOR_SOUND::FIRE_1);
+    this->sound->RegisterSound("Resource/Sound/VectorFire2.wav", KG::Sound::SoundType::EFFECTIVE, VECTOR_SOUND::FIRE_2);
+    this->sound->RegisterSound("Resource/Sound/VectorFire3.wav", KG::Sound::SoundType::EFFECTIVE, VECTOR_SOUND::FIRE_3);
+    this->sound->RegisterSound("Resource/Sound/VectorFire4.wav", KG::Sound::SoundType::EFFECTIVE, VECTOR_SOUND::FIRE_4);
+    this->sound->RegisterSound("Resource/Sound/VectorReload.wav", KG::Sound::SoundType::EFFECTIVE, VECTOR_SOUND::RELOAD);
+    this->sound->RegisterSound("Resource/Sound/VectorReloadEmpty.wav", KG::Sound::SoundType::EFFECTIVE, VECTOR_SOUND::RELOAD_EMPTY);
+
 
 	this->renderer->Initialize(renderDesc, renderSetting);
     this->renderer->SetEditUIRender(setting.isEditMode);
@@ -580,8 +589,15 @@ void KG::GameFramework::PostSceneFunction()
 			auto* snd = this->sound->GetNewSoundComponent();
 			snd->LinkSystem(this->sound->GetFmodSystem(), this->sound->GetChannel());
 			snd->RegisterSound(this->sound->GetSound(SOUND_EFF_SHOT), SOUND_EFF_SHOT);
-			snd->RegisterSound(this->sound->GetSound(SOUND_EFF_RELOAD), SOUND_EFF_RELOAD);
-			obj.AddComponent(snd);
+            snd->RegisterSound(this->sound->GetSound(SOUND_EFF_RELOAD), SOUND_EFF_RELOAD);
+            snd->RegisterSound(this->sound->GetSound(VECTOR_SOUND::DRAW), VECTOR_SOUND::DRAW);
+            snd->RegisterSound(this->sound->GetSound(VECTOR_SOUND::FIRE_1), VECTOR_SOUND::FIRE_1);
+            snd->RegisterSound(this->sound->GetSound(VECTOR_SOUND::FIRE_2), VECTOR_SOUND::FIRE_2);
+            snd->RegisterSound(this->sound->GetSound(VECTOR_SOUND::FIRE_3), VECTOR_SOUND::FIRE_3);
+            snd->RegisterSound(this->sound->GetSound(VECTOR_SOUND::FIRE_4), VECTOR_SOUND::FIRE_4);
+            snd->RegisterSound(this->sound->GetSound(VECTOR_SOUND::RELOAD), VECTOR_SOUND::RELOAD);
+            snd->RegisterSound(this->sound->GetSound(VECTOR_SOUND::RELOAD_EMPTY), VECTOR_SOUND::RELOAD_EMPTY);
+            obj.AddComponent(snd);
 		}
 		);
 
